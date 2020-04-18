6d868df506a1c720796099e668f2bc44c7b48c36
Erk <luffiez123@gmail.com>
2020-04-18 13:43:09 +0200

    temp level

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,21 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    AIPath aiPath;
+
+    void Start()
+    {
+        aiPath = GetComponent<AIPath>();               
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
