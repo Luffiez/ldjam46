8f7a168ae4209d1124b45572681f3ccfea397a01
IsBebs <banangurkkungen@gmail.com>
2020-04-18 19:30:38 +0200

    Now the gamehandler spawn flowers and enemies

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,12 +7,20 @@ public class GameHandler : MonoBehaviour
 {
     int Score = 0;
     UnityEvent GameOver;
+    [Header("Flower Settings")]
     [SerializeField]
+    float FlowerSpawnTime;
+    float FlowerSpawnTimer;
     Spawner FlowerSpawner;
+    [Header("Enemy settings")]
     [SerializeField]
+    float EnemySpawnTime;
+    float EnemySpawnTimer;
     Spawner EnemySpawner;
-
     static GameHandler Instance;
+
+
+
 
     private void Awake()
     {
@@ -21,11 +29,34 @@ public class GameHandler : MonoBehaviour
         else { Instance = this; }
     }
 
+    private void Start()
+    {
+        FlowerSpawnTimer = Time.time + FlowerSpawnTime;
+        EnemySpawnTimer = Time.time + EnemySpawnTimer;
+    }
+
+    private void Update()
+    {
+        if (FlowerSpawnTimer < Time.time)
+        {
+            FlowerSpawner.Spawn();
+            FlowerSpawnTimer = Time.time + FlowerSpawnTime;
+        }
+        if (EnemySpawnTimer < Time.time)
+        {
+            EnemySpawner.Spawn();
+            EnemySpawnTimer = Time.time + EnemySpawnTime;
+        }
+    }
+
     void AddScore(int _points)
     {
         Score += _points;
     }
 
-
+    void StartGameOver()
+    {
+        GameOver.Invoke();
+    }
 
 }
