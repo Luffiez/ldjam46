6664a343d4d6baf8ee2aa02d412cec829d583994
IsBebs <banangurkkungen@gmail.com>
2020-04-19 19:18:56 +0200

    Added a musicManager

---
+++ Assets/Prefabs/MusicManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+
+    public static MusicManager Instance;
+    [SerializeField]
+    float Volume;
+    AudioSource ASource;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        ASource = GetComponent<AudioSource>();
+        if (ASource.clip != null)
+            ASource.Play();
+        ASource.volume = Volume;
+    }
+
+
+    public void ChangeSong(AudioClip clip)
+    {
+        ASource.clip = clip;
+        ASource.Play();
+    }
+
+
+    public void ChangeVolume(float volume)
+    {
+        ASource.volume = volume;
+    }
+
+    public void StartMusic()
+    {
+        ASource.Play();
+    }
+
+    public void StopMusic()
+    {
+        ASource.Stop();
+    }
+    // Start is called before the first frame update
+
+}
