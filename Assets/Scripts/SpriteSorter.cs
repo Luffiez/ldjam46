59ecf3ae334674da4b3881c436d657d2de5ed9c8
IsBebs <banangurkkungen@gmail.com>
2020-04-19 19:35:03 +0200

    Made a sprite sorter

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+
+    [SerializeField]
+    int SortingBaseValue = 5000;
+    [SerializeField]
+    int OriginOffsetY;
+    [SerializeField]
+    bool RunOnce = false;
+    // Start is called before the first frame update
+    private Renderer SpriteRenderer;
+    void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate()
+    {
+        SpriteRenderer.sortingOrder = (int)(SortingBaseValue - transform.position.y - OriginOffsetY);
+        if (RunOnce)
+            Destroy(this);
+    }
+}
