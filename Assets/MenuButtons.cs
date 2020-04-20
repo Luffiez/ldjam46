a207024c2f0148d8f19d2515e2cfb5393db474d9
Erk <luffiez123@gmail.com>
2020-04-20 21:46:52 +0200

    Fixed final bug??

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+  
+    public void Play()
+    {
+        Transition.instance.Play();
+    }
+
+    public void Exit()
+    {
+        Transition.instance.Quit();
+    }
+
+}
